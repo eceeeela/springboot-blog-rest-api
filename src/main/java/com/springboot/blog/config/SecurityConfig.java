cd4e980891bff82650e443c0013edf05e75f9fd6
Yiling Chen <yiling.cn@gmail.com>
2024-12-11 20:32:31 -0500

    Build Login Rest Api

--- src/main/java/com/springboot/blog/config/SecurityConfig.java
+++ src/main/java/com/springboot/blog/config/SecurityConfig.java
@@ -46,6 +46,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests((authorize)->
                         //authorize.anyRequest().authenticated())
                         authorize.requestMatchers(HttpMethod.GET, "/api/**").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/api/auth/**").permitAll()
                                 .anyRequest().authenticated()
                 )
                 .httpBasic(Customizer.withDefaults());
