e7906eb7de9d555dc4d51d2379c6973e629c301c
Yiling Chen <yiling.cn@gmail.com>
2024-12-30 18:18:57 -0500

    Update JwtAuthenticationFilter.java

--- src/main/java/com/springboot/blog/security/JwtAuthenticationFilter.java
+++ src/main/java/com/springboot/blog/security/JwtAuthenticationFilter.java
@@ -4,7 +4,12 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -28,6 +33,24 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                                     FilterChain filterChain) throws ServletException, IOException {
 
         // get JWT token from Http request
+        String token = getTokenFromRequest(request);
+
+        // validate token
+        if(StringUtils.hasText(token) && jwtTokenProvider.validateToken(token)) {
+
+            // get username from token
+            String username = jwtTokenProvider.getUsername(token);
+
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                    userDetails,
+                    null,
+                    userDetails.getAuthorities()
+            );
+            authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
 
     }
 
